f11a1b06ebaaf8d6e3e5325eded3728859d7192e
Ismael Arias <ismaelariasmn@gmail.com>
2022-05-26 19:44:48 +0200

    Added userparser unit test.

---
+++ userparser/userparser_test.go
@@ -0,0 +1,15 @@
+package userparser
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetUsers(t *testing.T) {
+	var want []User
+	got := GetUsers()
+
+	if reflect.TypeOf(got) != reflect.TypeOf(want) {
+        t.Errorf("got %q, wanted %q", got, want)
+    }
+}
