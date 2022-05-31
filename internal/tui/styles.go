2ac14ec029176293ee316f2236dccc7caf8ad75c
Ismael Arias <ismaelariasmn@gmail.com>
2022-05-31 20:56:17 +0200

    Created new styles file to store all the variables related to styles.

---
+++ internal/tui/styles.go
@@ -0,0 +1,7 @@
+package tui
+
+import "github.com/charmbracelet/lipgloss"
+
+var (
+	docStyle = lipgloss.NewStyle().Margin(1, 2)
+)
--- internal/tui/view.go
+++ internal/tui/view.go
@@ -5,7 +5,6 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-var docStyle = lipgloss.NewStyle().Margin(1, 2)
 func (bu BubbleUser) View() string {
 	return docStyle.Render(bu.list.View())
 }
