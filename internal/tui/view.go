db8680c07de8db9c347a266af6d5adb49b3d7d86
Ismael Arias <ismaelariasmn@gmail.com>
2022-07-18 17:02:41 +0200

    Cleaning up the view functions.

--- internal/tui/view.go
+++ internal/tui/view.go
@@ -25,28 +25,16 @@ func (bu BubbleUser) listView() string {
 
 func (bu BubbleUser) detailView() string {
 	builder := &strings.Builder{}
-
 	divider := dividerStyle.Render(strings.Repeat("-", bu.viewport.Width)) + "\n"
 	detailsHeader := headerStyle.Render("Details")
 	memberOfHeader := headerStyle.Render("Member of")
 
 	if it := bu.list.SelectedItem(); it != nil {
-		username := fmt.Sprintf("\n\nUsername: %s\n", it.(item).Details.Username)
-		fullname := fmt.Sprintf("Fullname: %s\n", it.(item).Details.Name)
-		identificators := fmt.Sprintf("UID: %s\nGID: %s\n", it.(item).Details.Uid, it.(item).Details.Gid)
-		homeDirectory := fmt.Sprintf("Home directory: %s\n", it.(item).Details.HomeDir)
-
 		builder.WriteString(detailsHeader)
-		builder.WriteString(username)
-		builder.WriteString(fullname)
-		builder.WriteString(identificators)
-		builder.WriteString(homeDirectory)
-
+		builder.WriteString(renderUserDetails(it.(item).Details))
 		builder.WriteString(divider)
-
 		builder.WriteString(memberOfHeader)
 		builder.WriteString(fmt.Sprintf("\n\n%s", renderGroupTable(it.(item).Groups).View()))
-
 	}
 	details := wordwrap.String(builder.String(), bu.viewport.Width)
 
@@ -72,4 +60,13 @@ func renderGroupTable(groups []*user.Group) table.Model {
 		HeaderStyle(tableHeaderStyle)
 
 	return groupsTable
+}
+
+func renderUserDetails(user user.User) string {
+	username := fmt.Sprintf("\n\nUsername: %s\n", user.Username)
+	fullname := fmt.Sprintf("Fullname: %s\n", user.Name)
+	identificators := fmt.Sprintf("UID: %s\nGID: %s\n", user.Uid, user.Gid)
+	homeDirectory := fmt.Sprintf("Home directory: %s\n", user.HomeDir)
+	
+	return username+fullname+identificators+homeDirectory
 }
