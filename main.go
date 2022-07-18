d4b93a0e2a11dfe4fc6a34c5d469a7fbdf7cbf8a
Ismael Arias <ismaelariasmn@gmail.com>
2022-07-18 16:52:33 +0200

    Running go fmt.

--- main.go
+++ main.go
@@ -9,10 +9,10 @@ import (
 )
 
 func main() {
-    p := tea.NewProgram(tui.InitialModel(), tea.WithAltScreen())
-    
-    if err := p.Start(); err != nil {
-        fmt.Printf("Alas, there's been an error: %v", err)
-        os.Exit(1)
-    }
+	p := tea.NewProgram(tui.InitialModel(), tea.WithAltScreen())
+
+	if err := p.Start(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
 }
